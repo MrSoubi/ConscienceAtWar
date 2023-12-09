8bcf851f4ff76d2a2734852b14459adddfe02c0c
MrSoubi <122730595+MrSoubi@users.noreply.github.com>
2023-12-09 13:04:36 +0100

    Modified the includes and added the condition verification on choices.

--- ConscienceAtWar/Program.cpp
+++ ConscienceAtWar/Program.cpp
@@ -2,7 +2,7 @@
 
 #include <string>
 #include <iostream>
-
+#include <vector>
 #include "ReadFile.h"
 #include "Program.h"
 
@@ -14,6 +14,7 @@ int Program::karma = 0;
 std::vector<std::string> Program::inventory = std::vector<std::string>{};
 
 int main(int argc, char* argv[]) {
+	Program::inventory.push_back("");
 	std::vector<Scene> scenes = ReadFile::Start(argc, argv);
 
 	scenes[0].Display(scenes);
--- ConscienceAtWar/Scene.h
+++ ConscienceAtWar/Scene.h
@@ -1,12 +1,7 @@
 #pragma once
 
-#include <iostream>
 #include <string>
-
-#include <chrono>
-#include <future>
-#include <iomanip>
-#include <conio.h>
+#include <vector>
 
 #include "Paragraph.h"
 #include "Choice.h"
