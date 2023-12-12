0389c55b847bcc1a88d01af458b942c109c0cebf
Tristan Poveda <povedatristan@gmail.com>
2023-12-12 09:00:24 +0100

    Update Scene.cpp

--- ConscienceAtWar/Scene.cpp
+++ ConscienceAtWar/Scene.cpp
@@ -65,6 +65,7 @@ void Scene::Display(vector<Scene> scene) {
     auto end_time = start_time + std::chrono::milliseconds(timer * 1000);
     bool messageDisplayed = false;
 
+<<<<<<< Updated upstream
     while ((std::chrono::steady_clock::now() < end_time) && playerChoice == -1) {
         auto current_time = std::chrono::steady_clock::now();
         auto remaining_time = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - current_time).count() / 1000.0;
@@ -87,9 +88,44 @@ void Scene::Display(vector<Scene> scene) {
                     cout << inputNumber << ". "; choices[i].Display(writeSpeed);
                     inputNumber++;
                     choicesDisplayPos++;
+=======
+    while (std::chrono::steady_clock::now() < end_time) {
+        if (future.wait_for(std::chrono::milliseconds(0)) != std::future_status::ready) {
+            auto current_time = std::chrono::steady_clock::now();
+            auto remaining_time = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - current_time).count() / 1000.0;
+
+            for (int i = 0; i < paragraphs.size(); i++) {
+                if (paragraphs[i].displayed == false && paragraphs[i].timeOffSet > 0 && ConditionVerification(paragraphs[i].conditions)) {
+                    if (std::chrono::duration_cast<std::chrono::milliseconds>(current_time - start_time).count() >= paragraphs[i].timeOffSet * 1000) {
+                        paragraphs[i].displayed = true;
+                        MoveToConsoleLine(paragraphsDisplayPos);
+                        paragraphsDisplayPos++;
+                        paragraphs[i].Display(10);
+                        if (paragraphs[i].actions.size() > 0) ActivateActions(paragraphs[i].actions);
+                    }
                 }
             }
+            for (int i = 0; i < choices.size(); i++) {
+                if (choices[i].displayed == false && choices[i].timeOffSet > 0 && ConditionVerification(choices[i].conditions)) {
+                    if (std::chrono::duration_cast<std::chrono::milliseconds>(current_time - start_time).count() >= choices[i].timeOffSet * 1000) {
+                        choices[i].displayed = true;
+                        MoveToConsoleLine(choicesDisplayPos);
+                        std::cout << inputNumber << ". "; choices[i].Display(10);
+                        inputNumber++;
+                        choicesDisplayPos++;
+                    }
+>>>>>>> Stashed changes
+                }
+            }
+            // Affichage du temps restant
+            if (timer > 0) {
+                MoveToConsoleLine(timerDisplayPos);
+                std::cout << "\033[2K\r" << std::endl;
+                std::cout << "Temps restant : " << std::fixed << std::setprecision(2) << remaining_time << " secondes" << std::flush;
+            }
+            std::this_thread::sleep_for(std::chrono::milliseconds(1));
         }
+<<<<<<< Updated upstream
         // Affichage du temps restant
         moveToConsoleLine(timerDisplayPos);
         std::cout << "\033[2K\r" << std::endl;       
@@ -108,15 +144,32 @@ void Scene::Display(vector<Scene> scene) {
                     if (choices[n].link == scene[i].name) {
                         system("cls");
                         scene[i].Display(scene);
+=======
+        else if(future.wait_for(std::chrono::seconds(0)) == std::future_status::ready || timer <= 0) {
+            future.get();
+            for (int n = 0; n < choices.size(); n++) {
+                if (playerChoice == n + 1) {
+                    for (int i = 0; i < scene.size(); i++) {
+                        if (choices[n].link == scene[i].name) {
+                            if (choices[n].actions.size() > 0) ActivateActions(choices[n].actions);
+                            system("cls");
+                            scene[i].Display(scene);
+                        }
+>>>>>>> Stashed changes
                     }
                 }
             }
         }
     }
+<<<<<<< Updated upstream
     else if(playerChoice < inputNumber){
         if (timer > 0) {
             scene[2].Display(scene); //Fin du chrono
         }
+=======
+    if (timer > 0) {
+        scene[0].Display(scene); //Fin du chrono
+>>>>>>> Stashed changes
     }
     else {
         scene[2].Display(scene); //Mauvais input
