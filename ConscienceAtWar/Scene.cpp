6524859020d5c1bbdb0972bae32ae2041a653e2d
Tristan Poveda <povedatristan@gmail.com>
2023-12-12 09:00:53 +0100

    Update Scene.cpp

--- ConscienceAtWar/Scene.cpp
+++ ConscienceAtWar/Scene.cpp
@@ -1,24 +1,54 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <chrono>
+#include <future>
+#include <iomanip>
+#include <conio.h>
+
+#include "Paragraph.h"
+#include "Choice.h"
+
 #include "Scene.h"
-Scene::Scene(string name, vector<Paragraph> paragraphs, vector<Choice> choices, int timer) {
+
+
+Scene::Scene(std::string name, std::vector<Paragraph> paragraphs, std::vector<Choice> choices, int timer) {
 	this->name = name;
 	this->paragraphs = paragraphs;
 	this->choices = choices;
 	this->timer = timer;
 }
 
-void moveToConsoleLine(int line) {
+void MoveToConsoleLine(int line) {
     std::cout << "\033[" << line << ";0H";
 }
 
-void getUserChoice(int& playerChoice) {
+void GetUserChoice(int& playerChoice) {
     playerChoice = _getch() - '0';
 }
 
-void Scene::Display(vector<Scene> scene) {
-    bool inputed = false;
+bool ConditionVerification(std::vector<Condition> conditions) {
+    if (conditions.size() == 0) return true; // If there is no conditions, return true
 
-    int writeSpeed = 17;
+    bool result = true;
 
+    for (int i = 0; i < conditions.size(); i++)
+    {
+        result = result && conditions[i].IsVerified();
+    }
+
+    return result;
+}
+void ActivateActions(std::vector<Action> actions) {
+    for (int i = 0; i < actions.size(); i++)
+    {
+        actions[i].Activate();
+    }
+}
+
+void Scene::Display(std::vector<Scene> scene) {
     int timerDisplayPos = 2;
     int paragraphsDisplayPos = timerDisplayPos + 3;
     int choicesDisplayPos = paragraphsDisplayPos + 8;
@@ -26,69 +56,44 @@ void Scene::Display(vector<Scene> scene) {
     int inputNumber = 1;
 
     system("cls");
-    cout << "Scene : " << name<<endl;
+    std::cout << "Scene : " << name<< std::endl;
 
-    moveToConsoleLine(timerDisplayPos);
+    MoveToConsoleLine(timerDisplayPos);
 
     std::cout << "\033[2K\r" << std::endl;
-    cout << "Temps restant : " << timer << ".00 secondes";
+    std::cout << "Temps restant : " << timer << ".00 secondes";
 
-    moveToConsoleLine(paragraphsDisplayPos);
+    MoveToConsoleLine(paragraphsDisplayPos);
 
     for (int i = 0; i < paragraphs.size(); i++) {
         if (paragraphs[i].timeOffSet <= 0) {
-            paragraphs[i].Display(writeSpeed);
+            paragraphs[i].Display(10);
             paragraphsDisplayPos++;
         }
     }
 
-    moveToConsoleLine(choicesDisplayPos);
+    MoveToConsoleLine(choicesDisplayPos);
 
-    cout << "-----------------------------------------"<<endl;
+    std::cout << "-----------------------------------------"<< std::endl;
     choicesDisplayPos += 2;
 
-    moveToConsoleLine(choicesDisplayPos);
+    MoveToConsoleLine(choicesDisplayPos);
 
     for (int i = 0; i < choices.size(); i++) {
-        if (choices[i].timeOffSet <= 0) {          
-            cout << inputNumber << ". "; choices[i].Display(10);
+        if (choices[i].timeOffSet <= 0 && ConditionVerification(choices[i].conditions)) {
+            std::cout << inputNumber << ". "; choices[i].Display(10);
             inputNumber++;
             choicesDisplayPos++;
         }
     }
 
     int playerChoice = -1;
-
-    auto future = async(getUserChoice, ref(playerChoice));
+    auto future = std::async(GetUserChoice, std::ref(playerChoice));
 
     auto start_time = std::chrono::steady_clock::now();
     auto end_time = start_time + std::chrono::milliseconds(timer * 1000);
     bool messageDisplayed = false;
 
-<<<<<<< Updated upstream
-    while ((std::chrono::steady_clock::now() < end_time) && playerChoice == -1) {
-        auto current_time = std::chrono::steady_clock::now();
-        auto remaining_time = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - current_time).count() / 1000.0;
-
-        for (int i = 0; i < paragraphs.size(); i++) {
-            if (paragraphs[i].displayed == false && paragraphs[i].timeOffSet > 0) {
-                if (std::chrono::duration_cast<std::chrono::milliseconds>(current_time - start_time).count() >= paragraphs[i].timeOffSet * 1000) {
-                    paragraphs[i].displayed = true;
-                    moveToConsoleLine(paragraphsDisplayPos);
-                    paragraphsDisplayPos++;
-                    paragraphs[i].Display(writeSpeed);
-                }
-            }
-        }
-        for (int i = 0; i < choices.size(); i++) {
-            if (choices[i].displayed == false && choices[i].timeOffSet > 0) {
-                if (std::chrono::duration_cast<std::chrono::milliseconds>(current_time - start_time).count() >= choices[i].timeOffSet * 1000) {
-                    choices[i].displayed = true;
-                    moveToConsoleLine(choicesDisplayPos);
-                    cout << inputNumber << ". "; choices[i].Display(writeSpeed);
-                    inputNumber++;
-                    choicesDisplayPos++;
-=======
     while (std::chrono::steady_clock::now() < end_time) {
         if (future.wait_for(std::chrono::milliseconds(0)) != std::future_status::ready) {
             auto current_time = std::chrono::steady_clock::now();
@@ -114,7 +119,6 @@ void Scene::Display(vector<Scene> scene) {
                         inputNumber++;
                         choicesDisplayPos++;
                     }
->>>>>>> Stashed changes
                 }
             }
             // Affichage du temps restant
@@ -125,26 +129,6 @@ void Scene::Display(vector<Scene> scene) {
             }
             std::this_thread::sleep_for(std::chrono::milliseconds(1));
         }
-<<<<<<< Updated upstream
-        // Affichage du temps restant
-        moveToConsoleLine(timerDisplayPos);
-        std::cout << "\033[2K\r" << std::endl;       
-        std::cout << "Temps restant : " << std::fixed << std::setprecision(2) << remaining_time << " secondes" << std::flush;
-
-        
-
-        std::this_thread::sleep_for(std::chrono::milliseconds(1));
-    }
-
-    if ((future.wait_for(chrono::seconds(0)) == future_status::ready || timer <= 0) && playerChoice < inputNumber) { //Input du joueur
-        future.get();
-        for (int n = 0; n <= scene.size(); n++) {
-            if (playerChoice == n + 1) {
-                for (int i = 0; i < scene.size(); i++) {
-                    if (choices[n].link == scene[i].name) {
-                        system("cls");
-                        scene[i].Display(scene);
-=======
         else if(future.wait_for(std::chrono::seconds(0)) == std::future_status::ready || timer <= 0) {
             future.get();
             for (int n = 0; n < choices.size(); n++) {
@@ -155,23 +139,12 @@ void Scene::Display(vector<Scene> scene) {
                             system("cls");
                             scene[i].Display(scene);
                         }
->>>>>>> Stashed changes
                     }
                 }
             }
         }
     }
-<<<<<<< Updated upstream
-    else if(playerChoice < inputNumber){
-        if (timer > 0) {
-            scene[2].Display(scene); //Fin du chrono
-        }
-=======
     if (timer > 0) {
         scene[0].Display(scene); //Fin du chrono
->>>>>>> Stashed changes
-    }
-    else {
-        scene[2].Display(scene); //Mauvais input
     }
 }
